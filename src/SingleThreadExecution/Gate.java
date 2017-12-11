f034dfb620a4b7e766a74c670b85c6f758c8ae5d
Eoder <287629872@qq.com>
2017-12-11 10:47:50 +0800

    延长临界区，提高检查出错可能性

--- src/SingleThreadExecution/Gate.java
+++ src/SingleThreadExecution/Gate.java
@@ -4,13 +4,17 @@ public class Gate {
     private int counter = 0;
     private String name = "Nobody";
     private String address = "Nowhere";
-    public synchronized void pass(String name, String address) {
+    public void pass(String name, String address) {
         this.counter++;
         this.name = name;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
         this.address = address;
         check();
     }
-    public synchronized String toString() {
+    public String toString() {
         return "No." + counter + ": " + name + ", " + address;
     }
     private void check() {
