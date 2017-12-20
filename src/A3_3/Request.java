a752306564f2ba27e975519743e5b39108a5010f
Eoder <287629872@qq.com>
2017-12-20 15:43:12 +0800

    Debug of RequestQueue

---
+++ src/A3_3/Request.java
@@ -0,0 +1,15 @@
+package A3_3;
+
+
+public class Request {
+    private final String name;
+    public Request(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public String toString() {
+        return "[ Request " + name + " ]";
+    }
+}
